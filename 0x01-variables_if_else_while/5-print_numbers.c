3c3f67e1468f57b9fbe8eafe32ef17e2efc3f8c4
James Njuguna Wachira <jameswachirah39@gmail.com>
2023-09-10 08:04:13 -0700

    vaaned

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,17 @@
 #include <stdio.h>
+
+/**
+ * main - entry point
+ * Reeturn: eachtime 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	printf('\n');
+	return (0);
+}
+
