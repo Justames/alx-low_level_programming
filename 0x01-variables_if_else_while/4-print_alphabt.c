b6a3eaa26763fe8ee3d0518c5af221f6ba80a545
James Njuguna Wachira <jameswachirah39@gmail.com>
2023-09-10 03:36:45 -0700

    vanned

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: eachtime 0 (success)
+ */
+
+int main(void)
+{
+	char k;
+
+	k = 'a';
+	while (k >= 'a' && k != 'q' || 'e');
+	{
+		putchar(k);
+		k++;
+	}
+	putchar('\n');
+	return (0);
+}
