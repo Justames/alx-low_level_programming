e2d0ddacf265fd2a49d8a6fc7f745fe85caa8112
James Njuguna Wachira <jameswachirah39@gmail.com>
2023-09-18 17:29:53 -0700

    jug

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * swap_int: function that swaps the values of two integers
+ * swap_int - function that swaps the values of two integers
  * @a: integer to swap
- * @b: integerto swap
+ * @b: integer to swap
  */
 
 void swap_int(int *a, int *b)
