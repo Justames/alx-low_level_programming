17b2073ccb1618f568b3f86c30775c9402320b04
James Njuguna Wachira <jameswachirah39@gmail.com>
2023-09-21 20:23:28 -0700

    k

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,19 +10,19 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i;
-
+	int op;
+	op = 0;
 	i = 0;
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
 		{
-			return (s1[i] - s2[i];
+			return (s1[i] - s2[i]);
 		}
 		i++;
 	}
-	return (0)
+	return (0);
 
-	int i = 0, op = 0;
 	while (op == 0)
 	{
 		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,36 +1,26 @@
 #include "main.h"
-<<<<<<< HEAD
 
-/**
- * reverse_array - reverse array of integers
- * @a: array
- * @n: number of elements of array
- *
- * Return: void
-=======
 /**
  * reverse_array - reverses the content of an array
  * of integers.
  * @a: array.
  * @n: number of elements of the array.
- * Return: no return.
->>>>>>> 3c98b49b68ebe753d28626db2f63362c55f17d41
+ * Return: no return
  */
 
 void reverse_array(int *a, int n)
 {
-<<<<<<< HEAD
 	int i;
 	int t;
+	int j;
+	int temp;
 
 	for (i = 0; i < n--; i++)
 	{
 		t = a[i];
 		a[i] = a[n];
 		a[n] = t;
-=======
-	int i, j, temp;
-
+	}
 	for (i = 0; i < n - 1; i++)
 	{
 		for (j = i + 1; j > 0; j--)
@@ -39,6 +29,5 @@ void reverse_array(int *a, int n)
 			*(a + j) = *(a + (j - 1));
 			*(a + (j - 1)) = temp;
 		}
->>>>>>> 3c98b49b68ebe753d28626db2f63362c55f17d41
 	}
 }
