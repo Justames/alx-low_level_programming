93a54f16706351524ad586acb6b69ef180117c85
James Njuguna Wachira <jameswachirah39@gmail.com>
2023-09-25 01:15:53 -0700

    tot

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,4 @@
 #include "main.h"
-<<<<<<< HEAD
 
 /**
  * cap_string - capitalizes all words of a string
@@ -10,7 +9,9 @@
 
 char *cap_string(char *str)
 {
-	int index = 0;
+	int index;
+
+	index = 0;
 
 	while (str[index])
 	{
@@ -35,35 +36,4 @@ char *cap_string(char *str)
 	}
 
 	return (str);
-=======
-/**
- * cap_string - capitalizes all words of a string
- * @s: input string.
- * Return: the pointer to dest.
- */
-
-char *cap_string(char *s)
-{
-	int count = 0, i;
-
-	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
-
-	if (*(s + count) >= 97 && *(s + count) <= 122)
-		*(s + count) = *(s + count) - 32;
-	count++;
-	while (*(s + count) != '\0')
-	{
-		for (i = 0; i < 13; i++)
-		{
-			if (*(s + count) == sep_words[i])
-			{
-				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-					*(s + (count + 1)) = *(s + (count + 1)) - 32;
-				break;
-			}
-		}
-		count++;
-	}
-	return (s);
->>>>>>> 3c98b49b68ebe753d28626db2f63362c55f17d41
 }
--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,8 +1,7 @@
 #include "main.h"
-<<<<<<< HEAD
 
 /**
- * leet - encode into 1337speak
+ * *leet - encode into 1337speak
  * @n: input value
  * Return: n value
  */
@@ -24,32 +23,4 @@ char *leet(char *n)
 		}
 	}
 	return (n);
-=======
-/**
- * leet - encodes a string into 1337
- * @s: input string.
- * Return: the pointer to dest.
- */
-
-char *leet(char *s)
-{
-	int count = 0, i;
-	int low_letters[] = {97, 101, 111, 116, 108};
-	int upp_letters[] = {65, 69, 79, 84, 76};
-	int numbers[] = {52, 51, 48, 55, 49};
-
-	while (*(s + count) != '\0')
-	{
-		for (i = 0; i < 5; i++)
-		{
-			if (*(s + count) == low_letters[i] || *(s + count) == upp_letters[i])
-			{
-				*(s + count) = numbers[i];
-				break;
-			}
-		}
-		count++;
-	}
-	return (s);
->>>>>>> 3c98b49b68ebe753d28626db2f63362c55f17d41
 }
