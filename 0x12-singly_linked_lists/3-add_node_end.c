cd8db39f07c5cbb2b1809e56c51f04af1d758bac
Justames <jameswachirah39@gmal.com>
2023-10-25 12:37:58 -0700

    noti

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,39 +1,42 @@
-#include "lists.h"
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include "lists.h"
 
+/**
+ * add_node_end - adds a new node at the end of a linked list
+ * @head: double pointer to the list_t list
+ * @str: string to put in the new node
+ *
+ * Return: address of the new element, or NULL if it failed
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    if (str == NULL)
-        return NULL;
+ list_t *new;
+ list_t *temp = *head;
+ unsigned int len = 0;
 
-    list_t *new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return NULL;
+ while (str[len])
+ len++;
 
-    new_node->str = strdup(str);
-    if (new_node->str == NULL)
-    {
-        free(new_node);
-        return NULL;
-    }
+ new = malloc(sizeof(list_t));
+ if (!new)
+ return (NULL);
 
-    new_node->len = strlen(str);
-    new_node->next = NULL;
+ new->str = strdup(str);
+ new->len = len;
+ new->next = NULL;
 
-    if (*head == NULL)
-    {
-        *head = new_node;
-    }
-    else
-    {
-        list_t *temp = *head;
-        while (temp->next != NULL)
-        {
-            temp = temp->next;
-        }
-        temp->next = new_node;
-    }
+ if (*head == NULL)
+ {
+ *head = new;
+ return (new);
+ }
 
-    return new_node;
+ while (temp->next)
+ temp = temp->next;
+
+ temp->next = new;
+
+ return (new);
 }
+
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,14 +1,19 @@
-#include "lists.h"
 #include <stdlib.h>
+#include "lists.h"
 
+/**
+ * free_list - frees a linked list
+ * @head: list_t list to be freed
+ */
 void free_list(list_t *head)
 {
-    list_t *current = head;
-    while (current != NULL)
-    {
-        list_t *temp = current;
-        current = current->next;
-        free(temp->str);
-        free(temp);
-    }
+list_t *temp;
+
+while (head)
+	{
+	temp = head->next;
+	free(head->str);
+	free(head);
+	head = temp;
+	}
 }
