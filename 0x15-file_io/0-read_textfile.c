b368ab98928e72fa7d8f5e1a5b9056a1a22c833d
root <root@DESKTOP-BUGKGO7.>
2025-04-25 14:59:49 +0300

    ata mm sielewi

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,60 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <unistd.h>
 
-//write a function that reads a text file and prints it into the posix standard output
-//'letters' - number of letters it should read and print
-//return value = no of letters it could read and print
-//return 0 if file cant be opened or read
-//return 0 if filename is null
-//return 0 if write fails
-ssize_t read_textfile(const char *filename, size_t letters)
-{
+ssize_t read_textfile(const char *filename, size_t letters) {
     int fd;
     ssize_t read_bytes, written_bytes;
     char *buffer;
 
-    // Validate filename
-
+    /* Validate filename */
+    if (filename == NULL)
+    {
         return 0;
     }
 
-    // Open the file
+    /* Open the file */
     fd = open(filename, O_RDONLY);
     if (fd == -1)
     {
+        perror("Error opening file");
         return 0;
     }
 
-    // Allocate a buffer to hold the file content
+    /* Allocate memory for the buffer */
     buffer = malloc(sizeof(char) * letters);
-    if (buffer==NULL)
+    if (buffer == NULL)
     {
         close(fd);
+        perror("Error allocating memory");
         return 0;
     }
 
-    // Read the file into the buffer
+    /* Read the file */
     read_bytes = read(fd, buffer, letters);
     if (read_bytes == -1)
     {
         free(buffer);
         close(fd);
+        perror("Error reading file");
         return 0;
     }
 
-    // Write the buffer content to standard output
+    /* Write to standard output */
     written_bytes = write(STDOUT_FILENO, buffer, read_bytes);
     if (written_bytes == -1 || written_bytes != read_bytes)
     {
         free(buffer);
         close(fd);
+        perror("Error writing to stdout");
         return 0;
     }
 
-    // Clean up resources
+    /* Clean up */
     free(buffer);
     close(fd);
 
     return written_bytes;
 }
+
